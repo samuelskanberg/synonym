c1c21765b4b73d27247eb84b4d2eeb80c4bb3861
Samuel Skanberg <samuel.skanberg@gmail.com>
2014-12-28 22:55:06 +0100

    Show results after round

--- synonym.js
+++ synonym.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
     console.log("document loaded");
 
+    var wordlist = ["foo", "bar", "pizza", "kalsonger", "ananas", "bananer", "kall", "varm"];
+    var randomizer = 0;
+
     var team1_count = 0;
     var team2_count = 0;
     
     var current_team = 1;
     var countdown_counter = 5;
+
+    var current_word = "foo";
+    var correct_words = [];
+    var skipped_words = [];
 
     $("#startnewgame-button").click(function() {
         start_new_game();
@@ -30,13 +37,33 @@ $(document).ready(function() {
     function start_round(team_count) {
         console.log("Start round!");
         $("#roundpage").show();
-        countdown_counter = 5;
 
-        var random_word = get_random_word();
-        $("#currentword").html(random_word);
+        countdown_counter = 5;
+        correct_words = []
+
+        set_new_random_word();
 
         count_down();
         return false;
+    }
+
+    $("#nextword").click(function() {
+        correct_words.push(current_word);
+        set_new_random_word();
+
+        return false;
+    });
+
+    $("#skipword").click(function() {
+        skipped_words.push(current_word);
+        set_new_random_word();
+
+        return false;
+    });
+
+    function set_new_random_word() {
+        current_word = get_random_word();
+        $("#currentword").html(current_word);
     }
 
     function count_down() {
@@ -51,11 +78,26 @@ $(document).ready(function() {
     }
 
     function finish_round() {
-        alert("FINISHED");
+        console.log("Correct words:");
+        console.log(correct_words);
+
+        console.log("Skipped words:");
+        console.log(skipped_words);
+
+        var correct_words_string = correct_words.join(", "); 
+        $("#correctwords").html(correct_words_string);
+        $("#correctwordscount").val(correct_words.length);
+
+        var skipped_words_string = skipped_words.join(", "); 
+        $("#skippedwords").html(skipped_words_string);
+        $("#skippedwordscount").val(skipped_words.length);
+
+        $("#roundresultpage").show();
     }
 
     function get_random_word() {
-        return "pizza";
+        randomizer++;
+        return wordlist[randomizer%wordlist.length];
     }
 
 });
