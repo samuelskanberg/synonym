c7042301ff1980ac2d25eb954913c939c8c6635e
Samuel Skanberg <samuel.skanberg@gmail.com>
2014-12-28 21:52:17 +0100

    Added wordlist.php with some words and some javascript

--- synonym.js
+++ synonym.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     console.log("document loaded");
+
+    $("#startplay-button").click(function() {
+        $("#startpage").hide();
+        $("#playpage").show();
+    });
 });
---
+++ wordlist.php
@@ -0,0 +1,13 @@
+<?php
+
+$list = array();
+
+$list[] = "pizza";
+$list[] = "kartong";
+$list[] = "matbord";
+$list[] = "lampa";
+
+
+echo json_encode($list);
+
+?>
